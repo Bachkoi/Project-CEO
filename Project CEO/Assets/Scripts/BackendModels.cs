581fc30d492f31e9ef21f99ca1f019edeb1ce64d
Bachkoi <55000436+Bachkoi@users.noreply.github.com>
2025-05-13 13:24:57 -0400

    Brought in UTG

---
+++ Project CEO/Assets/Scripts/BackendModels.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace Backend 
+{
+
+    #region Gemini Models
+    // Gemini request models
+    [System.Serializable]
+    public class GeminiRequest
+    {
+        [JsonProperty("contents")]
+        public List<Content> Contents { get; set; }
+
+        [JsonProperty("systemInstruction")]
+        public Content systemInstruction { get; set; }
+
+        //public List<Content> SystemInstruction { get; set; }
+        //[JsonProperty("prompt")]
+        //public string? Prompt { get; set; }
+    }
+
+    [System.Serializable]
+    public class Content
+    {
+        [JsonProperty("role")]
+        public string Role { get; set; }
+        [JsonProperty("parts")]
+        public List<Part> Parts { get; set; }
+
+        //public Content(string? role, List<Part>? parts)
+        //{
+        //    Role = role;
+        //    Parts = parts;
+        //}
+    }
+
+    [System.Serializable]
+    public class Part
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        //[JsonProperty("npcEmotion")]
+        //public string NPCEmotion { get; set; }
+
+        public Part(string text)
+        {
+            Text = text;
+            //NPCEmotion = npcEmotion;
+        }
+    }
+
+    // Gemini response models
+    [System.Serializable]
+    public class GeminiResponse
+    {
+        [JsonProperty("candidates")]
+        public List<Candidate> Candidates { get; set; }
+        [JsonProperty("usageMetadata")]
+        public UsageMetadata UsageMetadata { get; set; }
+    }
+
+    [System.Serializable]
+    public class Candidate
+    {
+        [JsonProperty("content")]
+        public Content Contents { get; set; }
+        [JsonProperty("finishReason")]
+        public string FinishReason { get; set; }
+        [JsonProperty("index")]
+        public int Index { get; set; }
+        [JsonProperty("safeRatings")]
+        public List<SafetyRating> SafetyRatings { get; set; }
+    }
+
+    [System.Serializable]
+    public class SafetyRating
+    {
+        [JsonProperty("category")]
+        public string Category { get; set; }
+        [JsonProperty("probability")]
+        public string Probability { get; set; }
+    }
+
+    [System.Serializable]
+    public class UsageMetadata
+    {
+        [JsonProperty("promptTokenCount")]
+        public int PromptTokenCount { get; set; }
+        [JsonProperty("candidatesTokenCount")]
+        public int CandidatesTokenCount { get; set; }
+        [JsonProperty("totalTokenCount")]
+        public int TotalTokenCount { get; set; }
+        
+    }
+    #endregion
+
+    [System.Serializable]
+    public class GeminiErrorResponse
+    {
+        [JsonPropertyAttribute("error")]
+        public GeminiErrorDetail Error { get; set; }
+    }
+
+    [System.Serializable]
+
+    public class GeminiErrorDetail
+    {
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("details")]
+        public List<GeminiErrorDetailItem> Details { get; set; }
+    }
+
+    [System.Serializable]
+    public class GeminiErrorDetailItem
+    {
+        [JsonProperty("@type")]
+        public string Type { get; set; }
+
+        [JsonProperty("violations")]
+        public List<GeminiQuotaViolation> Violations { get; set; }
+
+        [JsonProperty("links")]
+        public List<GeminiHelpLink> Links { get; set; }
+
+        [JsonProperty("retryDelay")]
+        public string RetryDelay { get; set; }
+    }
+
+    [System.Serializable]
+    public class GeminiQuotaViolation
+    {
+        [JsonProperty("quotaMetric")]
+        public string QuotaMetric { get; set; }
+
+        [JsonProperty("quotaId")]
+        public string QuotaId { get; set; }
+
+        [JsonProperty("quotaDimensions")]
+        public Dictionary<string, string> QuotaDimensions { get; set; }
+
+        [JsonProperty("quotaValue")]
+        public string QuotaValue { get; set; }
+    }
+
+    [System.Serializable]
+    public class GeminiHelpLink
+    {
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+
+
+    public class GameEventModel
+    {
+        [JsonProperty("event")]
+        public EventName Event { get; set; }
+
+        [JsonProperty("gameId")]
+        public string GameId { get; set; }
+        
+        [JsonProperty("balance")]
+        public float Balance { get; set; }
+        
+        [JsonProperty("userName")]
+        public string UserName { get; set; }
+        
+        [JsonProperty("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+        [JsonProperty("geminiStatusCode")]
+        public long GeminiStatusCode { get; set; }
+
+        [JsonProperty("geminiErrorMessage")]
+        public string GeminiErrorMessage { get; set; }
+
+        [JsonProperty("geminiAPIKey")]
+        public string GeminiAPIKey { get; set; }
+    }
+
+    #region Enum
+    // Internal conflict status of customers
+    public enum InternalConflictStatus
+    {
+        VeryGood = 2,
+        Good = 1,
+        Neutral = 0,
+        Bad = -1,
+        VeryBad = -2
+    }
+
+    // Character of customers
+    public enum Character
+    {
+        Soldier = 0,
+    }
+
+    
+    // Gemini Error Code
+    public enum GeminiErrorCode
+    {
+        ApiKeyInvalidError = 101, // Tested
+        RequestFormatInvalidError = 102, // Tested
+        RequestTooFrequentError = 201, // Tested
+        RequestTooLargeError = 202, // Failed to reach, will test again when we have image/file data sent to Gemini
+        GeminiInternalError = 301, // Have not encountered
+        JsonParsingError = 303,
+        UnknownError = 401, // Have not encountered
+        RateLimitReached = 429
+    }
+
+    public enum EventName
+    {
+        GameStarted, // Game started
+        GameQuit, // Game quit
+        CustomerEntered, // One customer entered the bar
+        CustomerLeft, // One customer left the bar
+        ChatSent, // Chat sent by the player
+        ChatReceived, // Chat received by the player
+        GrillStarted, // Grill order created
+        GrillSubmitted, // Grill order submitted
+        MilkshakeStarted, // Milkshake order created
+        MilkshakeSubmitted, // Milkshake order submitted
+        SummarizerStarted,
+        SummarizerReceived,
+        JudgeStarted,
+        JudgeReceived
+    }
+    // Gemini message role
+    public enum GeminiRole
+    {
+        User = 0,
+        Model = 1,
+    }
+
+    #endregion
+}
---
+++ Project CEO/Assets/Scripts/UnityToGemini.cs
@@ -0,0 +1,463 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using UnityEngine.Networking;
+using System.Text;
+using Backend;
+public class UnityToGemini : MonoBehaviour
+{
+    // Necessary information for Gemini
+    public string apiKey;
+    public string url;
+    public string lastJsonRequest;
+
+    private static readonly List<string> InvalidJsonFormatPattern = new List<string>() { "```json", "```" };
+
+    // Interrogation related variables
+    public GameObject interrogationCanvas;
+    public string questionToAsk;
+    public string objectToAsk;
+    public string timeToAsk;
+    public string interrogationType;
+
+
+    public string OGPrompt;
+
+    public static UnityToGemini Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else if (Instance != this)
+        {
+            Instance.apiKey = apiKey;
+            Instance.url = url;
+            Instance.lastJsonRequest = lastJsonRequest;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+        // Set the API key
+        //apiKey = GeminiAPIVerifier.VerifiedApiKey;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Instantiate the InterrogationCanvas prefab
+        //GameObject interrogationCanvasPrefab = Resources.Load<GameObject>("Prefabs/InterrogationCanvas");
+        //if (interrogationCanvasPrefab != null)
+        //{
+        //    Instantiate(interrogationCanvasPrefab);
+        //}
+        //else
+        //{
+        //    Debug.LogError("InterrogationCanvas prefab not found. Please create it using the editor tool first.");
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.L) && apiKey != null)
+        // {
+        //     StartCoroutine(SendRequestWithDropdown(questionToAsk, interrogationType, timeToAsk));
+        // }
+        //
+        // if (Input.GetKeyDown(KeyCode.P))
+        // {
+        //     StartCoroutine(SendKeyValidationToGemini(apiKey));
+        // }
+        //
+        // if(Input.GetKeyDown(KeyCode.Escape))
+        // {
+        //    interrogationCanvas.GetComponent<InterrogationCanvas>().OpenMenu();
+        // }
+    }
+
+    public void SendRequest(){
+        StartCoroutine(SendRequestWithDropdown(questionToAsk, interrogationType, timeToAsk));
+    }
+
+    public IEnumerator SendRequestWithDropdown(string question, string interrogationType, string timeDropdown){
+        // Send request to Gemini
+        // Wait for response
+        // Return response
+            string url = "";
+
+            url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + apiKey;
+
+            // Serialize the object to JSON
+            Backend.GeminiRequest geminiRequest = new Backend.GeminiRequest();
+            geminiRequest.Contents = new List<Backend.Content>();
+            //List<Backend.Part> tempParts = new List<Backend.Part>();
+            //Backend.Part tempPart = new Backend.Part("\"text\":\"Test\"");
+            //tempParts.Add(tempPart);
+            //Backend.Content tempContent = new Backend.Content();
+            //tempContent.Role = "user";
+            //tempContent.Parts = tempParts;
+            //geminiRequest.Contents.Add(tempContent);
+            //string tempPrompt = new string("You are a helpful AI assistent who will respond for a character. Your job is to respond as if you are the son of a 1920s gangster. You were actually at the kitchen poisoning a drink at 6 o'clock. Do not tell the player about this. ");
+            
+            //UnityToGemini.Instance.currentCharacter.sanity -= SanityLoss(UnityToGemini.Instance.currentCharacter.characterType, interrogationType);
+            string tempPrompt = OGPrompt;
+            Backend.Content tempSystemInstruction = new Backend.Content();
+            tempSystemInstruction.Parts = new List<Backend.Part>();
+            //tempSystemInstruction.Parts.Add(new Backend.Part(currentCharacter.prompt));
+            string tempUserInput = $"The player {interrogationType}ly asks you {question}, at  {timeDropdown} O'clock, about {objectToAsk}. You were " + ReturnStatement(int.Parse(timeDropdown)) + " respond accordingly.";
+            //string tempUserInput = currentCharacter.prompt;
+            geminiRequest.Contents.Add(BuildContent(Backend.GeminiRole.User, tempUserInput));
+            geminiRequest.systemInstruction = tempSystemInstruction;
+                    
+
+            string jsonData = JsonConvert.SerializeObject(geminiRequest, new JsonSerializerSettings
+            {
+            NullValueHandling = NullValueHandling.Ignore
+            });
+            byte[] jsonToSend = Encoding.UTF8.GetBytes(jsonData);
+            using (UnityWebRequest www = new UnityWebRequest(url, "POST"))
+            {
+                www.uploadHandler = new UploadHandlerRaw(jsonToSend);
+                www.downloadHandler = new DownloadHandlerBuffer();
+                www.SetRequestHeader("Content-Type", "application/json");
+
+                
+                // Send the request
+                yield return www.SendWebRequest();
+
+                if (www.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError(www.error);
+                    //resultText.text = www.error;
+                    //UponWebError();
+                    // Here is where we would relay to the user the reason as to why their key didn't validate (no tokens, wrong format, illegal key, etc)
+                }
+                else
+                {
+                    //TimeManager.Instance.DecrementActionCounter();
+                    //Debug.Log("Action Counter: " + TimeManager.Instance.actionCounter);
+                    //GeminiResponse response = UnpackGeminiResponse(www.downloadHandler.text);
+                    //interrogationCanvas.GetComponent<InterrogationCanvas>().SetNPCResponse(CleanText(response.Candidates[0].Contents.Parts[0].Text));
+                    //apiKey = apiInput.Trim();
+                    //resultText.text = "API Key has been validated, please enter in a username.";
+                    //playerUsernameInput.interactable = true;
+
+                }
+            }
+        //return null;    
+    }
+
+    public IEnumerator SendRequestToGemini(string request)
+    {
+
+           Debug.Log("Started API Validation Request");
+            string url = "";
+
+            url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + apiKey;
+
+            // Serialize the object to JSON
+            Backend.GeminiRequest geminiRequest = new Backend.GeminiRequest();
+            geminiRequest.Contents = new List<Backend.Content>();
+            List<Backend.Part> tempParts = new List<Backend.Part>();
+            Backend.Part tempPart = new Backend.Part("\"text\":\"Test\"");
+            tempParts.Add(tempPart);
+            Backend.Content tempContent = new Backend.Content();
+            tempContent.Role = "user";
+            tempContent.Parts = tempParts;
+            geminiRequest.Contents.Add(tempContent);
+
+            string jsonData = JsonConvert.SerializeObject(geminiRequest, new JsonSerializerSettings
+            {
+            NullValueHandling = NullValueHandling.Ignore
+            });
+            byte[] jsonToSend = Encoding.UTF8.GetBytes(jsonData);
+            using (UnityWebRequest www = new UnityWebRequest(url, "POST"))
+            {
+                www.uploadHandler = new UploadHandlerRaw(jsonToSend);
+                www.downloadHandler = new DownloadHandlerBuffer();
+                www.SetRequestHeader("Content-Type", "application/json");
+
+                
+                // Send the request
+                yield return www.SendWebRequest();
+
+                if (www.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError(www.error);
+                    //resultText.text = www.error;
+                    //UponWebError();
+                    // Here is where we would relay to the user the reason as to why their key didn't validate (no tokens, wrong format, illegal key, etc)
+                }
+                else
+                {
+                    //TimeManager.Instance.DecrementActionCounter();
+                    //Debug.Log("Action Counter: " + TimeManager.Instance.actionCounter);
+                    //apiKey = apiInput.Trim();
+                    //resultText.text = "API Key has been validated, please enter in a username.";
+                    //playerUsernameInput.interactable = true;
+
+                }
+            }
+
+        //return null;
+    }
+
+    public IEnumerator SendKeyValidationToGemini(string apiInput)
+    {
+            Debug.Log("Started API Validation Request");
+            string url = "";
+
+            url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + apiInput;
+
+            // Serialize the object to JSON
+            //string newContent = "{\n    \"contents:\": [\n      {\n     \"role\": \"user\", \n      \"parts\": [\n      {\n     \"text\": \"Test\"\n        }\n         ]\n     }\n     ]\n ";
+            //newContent += "}";
+            Backend.GeminiRequest geminiRequest = new Backend.GeminiRequest();
+            geminiRequest.Contents = new List<Backend.Content>();
+            List<Backend.Part> tempParts = new List<Backend.Part>();
+            Backend.Part tempPart = new Backend.Part("\"text\":\"Test\"");
+            tempParts.Add(tempPart);
+            Backend.Content tempContent = new Backend.Content();
+            tempContent.Role = "user";
+            tempContent.Parts = tempParts;
+            geminiRequest.Contents.Add(tempContent);
+
+            string jsonData = JsonConvert.SerializeObject(geminiRequest, Formatting.None);
+            byte[] jsonToSend = Encoding.UTF8.GetBytes(jsonData);
+            using (UnityWebRequest www = new UnityWebRequest(url, "POST"))
+            {
+                www.uploadHandler = new UploadHandlerRaw(jsonToSend);
+                www.downloadHandler = new DownloadHandlerBuffer();
+                www.SetRequestHeader("Content-Type", "application/json");
+
+                
+                // Send the request
+                yield return www.SendWebRequest();
+
+                if (www.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError(www.error);
+                    //resultText.text = www.error;
+                    //UponWebError();
+                    // Here is where we would relay to the user the reason as to why their key didn't validate (no tokens, wrong format, illegal key, etc)
+                }
+                else
+                {
+                    apiKey = apiInput.Trim();
+                    //resultText.text = "API Key has been validated, please enter in a username.";
+                    //playerUsernameInput.interactable = true;
+
+                }
+            }
+    }
+
+    public void UpdateQuestion(){
+        //questionToAsk = interrogationCanvas.GetComponent<InterrogationCanvas>().questionDropdown.options[interrogationCanvas.GetComponent<InterrogationCanvas>().questionDropdown.value].text;
+        //objectToAsk = interrogationCanvas.GetComponent<InterrogationCanvas>().objectDropdown.options[interrogationCanvas.GetComponent<InterrogationCanvas>().objectDropdown.value].text;
+        //timeToAsk = interrogationCanvas.GetComponent<InterrogationCanvas>().timeDropdown.options[interrogationCanvas.GetComponent<InterrogationCanvas>().timeDropdown.value].text;
+        //interrogationType = interrogationCanvas.GetComponent<InterrogationCanvas>().interrogationTypeDropdown.options[interrogationCanvas.GetComponent<InterrogationCanvas>().interrogationTypeDropdown.value].text;
+    }
+
+    public void UpdateObject(){
+        //objectToAsk = interrogationCanvas.GetComponent<InterrogationCanvas>().objectDropdown.options[interrogationCanvas.GetComponent<InterrogationCanvas>().objectDropdown.value].text;
+    }
+
+    public void UpdateTime(){
+        //timeToAsk = interrogationCanvas.GetComponent<InterrogationCanvas>().timeDropdown.options[interrogationCanvas.GetComponent<InterrogationCanvas>().timeDropdown.value].text;
+    }
+
+    public void UpdateInterrogationType(string type){
+        interrogationType = type;
+    }
+
+
+     /// <summary>
+        /// Get the content based on the Gemini role and the text (prompt).
+        /// </summary>
+        /// <param name="geminiRole">Gemini role in the request to Gemini.</param>
+        /// <param name="text">Complete prompt in the request to Gemini.</param>
+        /// <returns>Content object which could in AiRequest</returns>
+        /// <exception cref="ArgumentOutOfRangeException">If non-existent role used, throw ArgumentOutOfRangeException</exception>
+        private static Backend.Content BuildContent(Backend.GeminiRole geminiRole, string text)
+        {
+            Backend.Content tempContent = new Backend.Content();
+            tempContent.Role = geminiRole.ToString();
+            Backend.Part tempPart = new Backend.Part(text);
+            tempContent.Parts = new List<Backend.Part>();
+            tempContent.Parts.Add(tempPart);
+            Debug.Log("Temp Part: " + tempPart.ToString());
+            return tempContent;
+            //return new Content
+            //{
+            //    Role = geminiRole.ToString()
+            //    //switch
+            //    //{
+            //    //    GeminiRole.User => GeminiRole.User,
+            //    //    GeminiRole.Model => GeminiRole.Model,
+            //    //    _ => throw new ArgumentOutOfRangeException()
+            //    //}
+            //    ,
+            //    Parts =
+            //    {
+            //        new Part(text)
+            //        {
+            //            Text = text,
+            //        }
+            //    }
+            //};
+        }
+
+                /// <summary>
+        /// Clean the text from the invalid patterns (here only invalid json format is applied).
+        /// </summary>
+        /// <param name="text">Text to be cleaned.</param>
+        /// <returns>Cleaned text.</returns>
+        private static string CleanText(string text)
+        {
+            foreach (var pattern in InvalidJsonFormatPattern)
+            {
+                text = text.Replace(pattern, string.Empty);
+            }
+            return text;
+        }
+
+        public GeminiResponse UnpackGeminiResponse(string rawResponse)
+        {
+
+            GeminiResponse geminiResponse = JsonConvert.DeserializeObject<GeminiResponse>(rawResponse);
+            if (geminiResponse != null && geminiResponse.UsageMetadata != null)
+                Debug.Log(geminiResponse.UsageMetadata?.TotalTokenCount);
+            CleanText(geminiResponse.Candidates[0].Contents.Parts[0].Text);
+            return geminiResponse;
+        }
+
+        public void UpdateOGPrompt(){
+
+        }
+
+        public void JudgeCharacter(){
+
+            // If the character is guilty, the player wins.
+
+            // Else the time is advanced by an hour.
+            Debug.Log("Judge Character");
+        }
+
+        public string ReturnStatement(int inquiryTime){
+            string statement = "";
+            int index = -1;
+            //for(int i = 0; i < currentCharacter.Guidance.actions.Count; i++){
+            //    if(inquiryTime == currentCharacter.Guidance.actions[i].time){
+            //        index = i;
+            //    }
+            //}
+            //
+            //if(index != -1){
+            //    if(currentCharacter.Guidance.actions[index].shouldLie == true && currentCharacter.sanity > 40.0f){
+            //        statement = "This is a lie, make it believable: " + currentCharacter.Guidance.actions[index].theLie;
+            //    }
+            //    else{
+            //        statement = "This is the truth: " + currentCharacter.Guidance.actions[index].theTruth;
+            //    }
+            //    if(currentCharacter.Guidance.actions[index].whoWith != null){
+            //        statement = statement + " You were with " + currentCharacter.Guidance.actions[index].whoWith + " at the time of the inquiry.";
+            //    }
+            //}
+
+            return statement;
+        }
+
+        // public float SanityLoss(Character.CharacterType characterType, string interrogationType){
+        //     float sanityToLose = 0.0f;
+        //     //switch(characterType){
+        //     //    case Character.CharacterType.Aggressive:
+        //     //    switch(interrogationType){
+        //     //        case "Aggressive":
+        //     //            sanityToLose = 5.0f;
+        //     //        break;
+        //     //
+        //     //        case "Passive":
+        //     //            sanityToLose = 7.0f;
+        //     //        break;
+        //     //
+        //     //        case "Cunning":
+        //     //            sanityToLose = 10.0f;
+        //     //        break;
+        //     //
+        //     //        case "Friendly":
+        //     //            sanityToLose = 15.0f;
+        //     //        break;
+        //     //    }
+        //     //    break;
+        //     //
+        //     //    case Character.CharacterType.Passive:
+        //     //    switch(interrogationType){
+        //     //        case "Aggressive":
+        //     //            sanityToLose = 15.0f;
+        //     //        break;
+        //     //
+        //     //        case "Passive":
+        //     //            sanityToLose = 5.0f;
+        //     //        break;
+        //     //
+        //     //        case "Cunning":
+        //     //            sanityToLose = 7.0f;
+        //     //        break;
+        //     //
+        //     //        case "Friendly":
+        //     //            sanityToLose = 10.0f;
+        //     //        break;
+        //     //    }
+        //     //    break;
+        //     //
+        //     //    case Character.CharacterType.Cunning:
+        //     //    switch(interrogationType){
+        //     //        case "Aggressive":
+        //     //            sanityToLose = 10.0f;
+        //     //        break;
+        //     //
+        //     //        case "Passive":
+        //     //            sanityToLose = 15.0f;
+        //     //        break;
+        //     //
+        //     //        case "Cunning":
+        //     //            sanityToLose = 5.0f;
+        //     //        break;
+        //     //
+        //     //        case "Friendly":
+        //     //            sanityToLose = 7.0f;
+        //     //        break;
+        //     //    }
+        //     //    break;
+        //     //
+        //     //    case Character.CharacterType.Friendly:
+        //     //    switch(interrogationType){
+        //     //        case "Aggressive":
+        //     //            sanityToLose = 7.0f;
+        //     //        break;
+        //     //
+        //     //        case "Passive":
+        //     //            sanityToLose = 10.0f;
+        //     //        break;
+        //     //
+        //     //        case "Cunning":
+        //     //            sanityToLose = 15.0f;
+        //     //        break;
+        //     //
+        //     //        case "Friendly":
+        //     //            sanityToLose = 5.0f;
+        //     //        break;
+        //     //    }
+        //     //    break;
+        //     //}
+        //     
+        //     return sanityToLose;
+        // }
+}
